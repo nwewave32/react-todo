cdb92eb79dee3394a07e1118ce429c48a68dcad0
nwewave32 <nwewave.32@gmail.com>
2024-07-18 06:57:46 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    setTodos([...todos, input]);
+    setInput('');
+  };
+
+  return (
+    <div className="App">
+      <h1>React To-Do List</h1>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
